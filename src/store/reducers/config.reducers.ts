c382a0247eb8d75e394c9d5205f73d428a801a16
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2019-10-19 20:18:23 +0300

    feat: added reducers

---
+++ src/store/reducers/config.reducers.ts
@@ -0,0 +1,18 @@
+import { IConfigState, initialConfigState } from "../states/config.state";
+import { ConfigActions, EConfigActions } from "../actions/config.action";
+
+export const configReducers = (
+  state = initialConfigState,
+  action: ConfigActions
+): IConfigState => {
+  switch (action.type) {
+    case EConfigActions.GetConfigSuccess: {
+      return {
+        ...state,
+        config: action.payload
+      };
+    }
+    default:
+      return state;
+  }
+};
---
+++ src/store/reducers/user.reducers.ts
@@ -0,0 +1,23 @@
+import { IUserState, initialUserState } from "../states/user.state";
+import { UserActions, EUserActions } from "../actions/user.action";
+
+export const userReducers = (
+  state = initialUserState,
+  action: UserActions
+): IUserState => {
+    switch(action.type){
+        case EUserActions.GetUsersSuccess: {
+            return {
+                ...state,
+                users: action.payload
+            };
+        }
+        case EUserActions.GetUserSuccess: {
+            return {
+                ...state,
+                selectedUser: action.payload
+            }
+        }
+        default: return state;
+    }
+};
